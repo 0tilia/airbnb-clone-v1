ee5097768449a99f65a004fade2c70cbf1b1668a
Otilia Toma <otilia@192-168-0-132.rdsnet.ro>
2023-12-05 22:39:18 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+import React from "react"
+import Navbar from './components/Navbar'
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+        <div>
+        <Navbar />
+        </div>
+    )
+}
