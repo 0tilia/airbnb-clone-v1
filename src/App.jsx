0629b729251bf2a552f7408cb7c667e7fec89b9b
Otilia Toma <otilia@192-168-0-132.rdsnet.ro>
2023-12-06 21:12:43 +0200

    navbar and main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './style.css'
 import React from "react"
 import Navbar from './components/Navbar'
+import Hero from "./components/Hero"
 
 /*
 Challenge: Build the Navbar component.
@@ -11,6 +12,7 @@ export default function App() {
     return (
         <div>
         <Navbar />
+        <Hero />
         </div>
     )
 }
